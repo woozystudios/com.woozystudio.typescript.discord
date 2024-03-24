8ca9aff873f796abe0426d75f8cca83c18557a2c
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-24 17:49:31 -0600

    Add commands

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,6 +1,7 @@
-import { Events } from "discord.js";
+import { Collection, Events, REST, Routes } from "discord.js";
 import CustomClient from "../../classes/CustomClient";
 import Event from "../../classes/Event";
+import Command from "../../classes/Command";
 
 export default class Ready extends Event {
     constructor(client: CustomClient) {
@@ -11,7 +12,33 @@ export default class Ready extends Event {
         })
     }
 
-    Execute() {
-        console.log(`Logged as ${this.client.user?.tag}`)
+    async Execute() {
+        console.log(`Logged as ${this.client.user?.tag}`);
+
+        const commands: object[] = this.GetJson(this.client.commands);
+
+        const rest = new REST().setToken(this.client.config.token);
+
+        const setCommands: any = await rest.put(Routes.applicationGuildCommands(this.client.config.discordClientId, this.client.config.guildId), {
+            body: commands
+        });
+
+        console.log(`Successfully set ${setCommands.length} commands!`);
+    }
+
+    private GetJson(commands: Collection<string, Command>): object[] {
+        const data: object[] = [];
+
+        commands.forEach(command => {
+            data.push({
+                name: command.name,
+                description: command.description,
+                options: command.options,
+                userPermissions: command.userPermissions.toString(),
+                dmPermissions: command.dmPermissions,
+            })
+        });
+
+        return data;
     }
 }
