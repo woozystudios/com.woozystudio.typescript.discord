b2f050ca077fc3e4a14038623caebb5f57e79918
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-24 17:50:44 -0600

    Add commands and subcommands

--- src/interfaces/ICustomClient.ts
+++ src/interfaces/ICustomClient.ts
@@ -1,7 +1,13 @@
+import { CollectedInteraction, Collection } from "discord.js";
 import IConfig from "./IConfig";
+import Command from "../classes/Command";
+import SubCommand from "../classes/SubCommand";
 
 export default interface ICustomClient {
     config: IConfig;
+    commands: Collection<string, Command>;
+    subCommands: Collection<string, SubCommand>;
+    cooldowns: Collection<string, Collection<string, number>>;
 
     Init(): void;
     LoadHandlers(): void;
