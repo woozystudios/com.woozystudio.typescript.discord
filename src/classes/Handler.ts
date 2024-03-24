cdf9a33a922da939ee0504418ff874510cd8a095
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-24 17:49:05 -0600

    Add command declarations

--- src/classes/Handler.ts
+++ src/classes/Handler.ts
@@ -3,19 +3,23 @@ import path from 'path';
 import { glob } from 'glob';
 import CustomClient from "./CustomClient";
 import Event from "./Event";
+import SubCommand from "./SubCommand";
+import Command from "./Command";
 
 export default class Handler implements IHandler {
     client: CustomClient;
     constructor(client: CustomClient) {
         this.client = client;
     }
+
     async LoadEvents() {
         const files = (await glob(`build/events/**/*.js`)).map(filePath => path.resolve(filePath))
 
         files.map(async (file: string) => {
             const event: Event = new(await import(file)).default(this.client);
 
-            if(!event.name) return delete require.cache[require.resolve(file)] && console.log(`${file.split("/").pop()} does not have a valid name.`);
+            if(!event.name)
+                return delete require.cache[require.resolve(file)] && console.log(`${file.split("/").pop()} does not have a valid name.`);
 
             const execute = (...args: any) => event.Execute(...args);
 
@@ -28,4 +32,24 @@ export default class Handler implements IHandler {
         });
     }
     
+    async LoadCommands() {
+        const files = (await glob(`build/commands/**/*.js`)).map(filePath => path.resolve(filePath));
+
+        files.map(async (file: string) => {
+            const command: Command | SubCommand = new(await import(file)).default(this.client);
+
+            if(!command.name)
+                return delete require.cache[require.resolve(file)] && console.log(`${file.split("/").pop()} does not have a valid name.`);
+
+            this.client.commands.set(command.name, command as Command);
+
+
+            if(file.split("/").pop()?.split(".")[2])
+                return this.client.subCommands.set(command.name, command);
+            
+            return delete require.cache[require.resolve(file)]
+
+            
+        });
+    }
 }
