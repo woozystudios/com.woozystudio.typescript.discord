5223c82a18771aebefc30f3d9c285be3916cae81
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-24 17:51:13 -0600

    Create Test.One.ts subcommand

---
+++ src/commands/Test.One.ts
@@ -0,0 +1,17 @@
+import { ApplicationCommandOptionType, CacheType, ChatInputCommandInteraction, PermissionsBitField } from "discord.js";
+import Command from "../classes/Command";
+import CustomClient from "../classes/CustomClient";
+import Category from "../enums/Category";
+import SubCommand from "../classes/SubCommand";
+
+export default class TestOne extends SubCommand {
+    constructor(client: CustomClient) {
+        super(client, {
+            name: "test.one"
+        });
+    }
+
+    Execute(interaction: ChatInputCommandInteraction) {
+        interaction.reply({ content: "Test one command has been ran!", ephemeral: true })
+    }
+}
