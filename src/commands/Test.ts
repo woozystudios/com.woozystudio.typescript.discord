737cdc8e9b556e5147d6c3089186167c09845084
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-24 17:51:03 -0600

    Create Test.ts command

---
+++ src/commands/Test.ts
@@ -0,0 +1,26 @@
+import { ApplicationCommandOptionType, CacheType, ChatInputCommandInteraction, PermissionsBitField } from "discord.js";
+import Command from "../classes/Command";
+import CustomClient from "../classes/CustomClient";
+import Category from "../enums/Category";
+
+export default class Test extends Command {
+    constructor(client: CustomClient) {
+        super(client, {
+            name: "test",
+            description: "My test command",
+            category: Category.Utilities,
+            userPermissions: PermissionsBitField.Flags.UseApplicationCommands,
+            dmPermissions: false,
+            cooldown: 3,
+            options: [{
+                name: "one",
+                description: "A",
+                type: ApplicationCommandOptionType.Subcommand
+            }]
+        });
+    }
+
+    // Execute(interaction: ChatInputCommandInteraction) {
+    //     interaction.reply({ content: "Test command has been ran!", ephemeral: true })
+    // }
+}
