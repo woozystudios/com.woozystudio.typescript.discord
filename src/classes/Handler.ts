6e1705396453724cd5aa260282403e9ebb8e49ce
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-24 14:22:51 -0600

    Create Handler.ts

---
+++ src/classes/Handler.ts
@@ -0,0 +1,31 @@
+import IHandler from "../interfaces/IHandler";
+import path from 'path';
+import { glob } from 'glob';
+import CustomClient from "./CustomClient";
+import Event from "./Event";
+
+export default class Handler implements IHandler {
+    client: CustomClient;
+    constructor(client: CustomClient) {
+        this.client = client;
+    }
+    async LoadEvents() {
+        const files = (await glob(`build/event/**/*.js`)).map(filePath => path.resolve(filePath))
+
+        files.map(async (file: string) => {
+            const event: Event = new(await import(file)).default(this.client);
+
+            if(!event.name) return delete require.cache[require.resolve(file)] && console.log(`${file.split("/").pop()} does not have a valid name.`);
+
+            const execute = (...args: any) => event.Execute(...args);
+
+            //@ts-ignore
+            if(event.once) this.client.once(event.name, execute);
+            //@ts-ignore
+            else this.client.on(event.name, execute);
+
+            return delete require.cache[require.resolve(file)]
+        });
+    }
+    
+}
