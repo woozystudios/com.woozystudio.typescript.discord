7687fc5c3c924738ee6bd5142c3d7581810d8613
Woozy's Studio <woozysstudio23@gmail.com>
2024-03-24 19:41:01 -0600

    Add global commands and development mode

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -2,6 +2,7 @@ import { Collection, Events, REST, Routes } from "discord.js";
 import CustomClient from "../../classes/CustomClient";
 import Event from "../../classes/Event";
 import Command from "../../classes/Command";
+import 'colors';
 
 export default class Ready extends Event {
     constructor(client: CustomClient) {
@@ -13,17 +14,33 @@ export default class Ready extends Event {
     }
 
     async Execute() {
-        console.log(`Logged as ${this.client.user?.tag}`);
+        console.log(`Logged as ${this.client.user?.tag}`.green);
 
-        const commands: object[] = this.GetJson(this.client.commands);
-
+        const clientId = this.client.developmentMode ? this.client.config.devDiscordClientId : this.client.config.discordClientId;
         const rest = new REST().setToken(this.client.config.token);
+
+        if(!this.client.developmentMode) {
+            const globalCommands: any = await rest.put(Routes.applicationCommands(clientId), {
+                body: this.GetJson(this.client.commands.filter(command => !command.development))
+            });
+
+            console.log(`Successfully loaded ${globalCommands.length} global application commands.`.yellow)
+        }
+
+        const devCommands: any = await rest.put(Routes.applicationGuildCommands(clientId, this.client.config.devGuildId), {
+            body: this.GetJson(this.client.commands.filter(command => command.development))
+        });
+
+        console.log(`Successfully loaded ${devCommands.length} development application commands.`.blue)
+
+
+        /* const commands: object[] = this.GetJson(this.client.commands);
 
         const setCommands: any = await rest.put(Routes.applicationGuildCommands(this.client.config.discordClientId, this.client.config.guildId), {
             body: commands
         });
 
-        console.log(`Successfully set ${setCommands.length} commands!`);
+        console.log(`Successfully set ${setCommands.length} commands!`.yellow); */
     }
 
     private GetJson(commands: Collection<string, Command>): object[] {
